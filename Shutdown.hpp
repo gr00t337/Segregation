1e6ebf87cfc8ee8738756d62e383e470a6d1004d
gr00t337 <137590078+gr00t337@users.noreply.github.com>
2025-04-21 16:49:12 +0700

    Delete Shutdown.hpp

--- Shutdown.hpp
+++
@@ -1,6 +0,0 @@
-void* Original_Shutdown_Caller;
-
-void __thiscall Redirected_Shutdown(void* Unknown_Parameter, char* Reason)
-{
-	(decltype(&Redirected_Shutdown)(Original_Shutdown_Caller))(Unknown_Parameter, (char*)"Segregation");
-}
